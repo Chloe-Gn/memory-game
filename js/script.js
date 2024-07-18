0cf4428a65fc63c51c4faa4b56cbd95cfea77720
Chloe-Gn <chloe.gn29@gmail.com>
2024-07-18 09:43:12 +0200

    My commit of the day

--- js/script.js
+++ js/script.js
@@ -1,59 +1,97 @@
 const cards = document.querySelectorAll(".card");
 
+document.body.onkeyup = function(e){
+    if(e.keyCode == 32){
+      location.reload();
+    }
+  };
+
+  const numberOfAttempts = document.getElementById("number-of-attempts");
+  
+
 let isFlippedCard = false;
 let firstCard, secondCard;
-let lockboard = false;
-
-
-console.log(cards)
+let lockBoard = false;
+let count = 0;
 
 function flipCard() {
-    this.classList.add('flip');
-    if (!isFlippedCard) {
-        isFlippedCard = true;
-        firstcard = this;
-        return;
-    }  else {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+  if (!isFlippedCard) {
+    isFlippedCard = true;
+    firstCard = this;
+    console.log("I am first card: " + firstCard)
+    return;
+  } else {
     secondCard = this;
-    isFlippedCard = false;
+    console.log("I am second card: " + secondCard)
     checkForMatch();
-        }
+    console.log("I am first card: " + firstCard)
+    console.log("I am second card: " + secondCard);
+    count = incrementCount();
+    displayCount();
+  }
 }
+
+
+const displayCount = () => {
+  setTimeout(() => {
+    numberOfAttempts.innerHTML = count;
+   }, 500);
+  }
+
+
+const incrementCount = () => {
+    return ++count ;
+  }
 
 
 
 const checkForMatch = () => {
-   let isMatch = firstCard.dataset.fruit === secondCard.dataset.fruit; isMatch ? disableCards() : unflipCards();
-}
+  let isMatch = firstCard.dataset.fruit === secondCard.dataset.fruit;
+  isMatch ? disableCards() : unflipCards();
+};
 
 const unflipCards = () => {
-    setTimeOut(() => {
-        firstCard.classList.remove('flip');
-        secondCard.classList.remove('flip');
-    }, 1500);
-}
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1500);
+};
 
+function disableCards () {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+};
 
+const resetBoard = () => {
+  isFlippedCard = false;
+  lockBoard = false;
+  firstCard = null;
+  secondCard = null;
+};
 
-const disableCards = () => {
-     firstCard.removeEventListener('Click',flipCard);
-    secondCard.removeEventListener('Click',flipCard);
-}
+const shuffle = () => {
+  cards.forEach((card) => {
+    let randomOrder = Math.floor(Math.random() * 12);
+    card.style.order = randomOrder;
+  });
+};
 
-   
+shuffle();
 
+// IIFE: Immediately invoked function element
 
-// let flipCard = (event) => {
-//     const card = event.currentTarget;
-//     card.classList.add('flip');
-//         if (!isFlippedCard) {
-//             isFlippedCard = true;
-//             firstCard = card;
-//         }
-// };
+// (function shuffle() {
+//     cards.forEach(card => {
+//         let randomOrder = Math.floor(Math.random() * 12);
+//         card.style.order = randomOrder;
+//     });
+// })();
 
-
-cards.forEach(card => card.addEventListener('click',flipCard));
-
-
+cards.forEach(card => card.addEventListener("click", flipCard));
 
