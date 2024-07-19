1035bf06969b44a569a44cf5ca892f604ca0f10f
Chloe-Gn <chloe.gn29@gmail.com>
2024-07-19 13:30:38 +0200

    Last commit

--- js/script.js
+++ js/script.js
@@ -107,76 +107,103 @@ cards.forEach(card => card.addEventListener("click", flipCard));
 // Beginning of form
 
 
-const $form = getElementbyID("subscription-form")
+document.getElementById('subscription-form').addEventListener('submit', verifyFormInputs);
 
 function verifyFormInputs(event) {
-  const errors = []
-  const user = {}
+    event.preventDefault(); 
+
+    const form = event.target;
+    const formData = new FormData(form);  
+
+    formData.forEach((blabla, lolo) => {
+        console.log(`${lolo}: ${blabla}`);  
+    });
+  };
+
+
+
+
+
+
+
+// $form.addEventListener("input",verifyFormInputs);
+
+// function verifyFormInputs(event) {
+//   const errors = []
+//   const user = {}
   
-  event.preventDefault()
-
-  const $inputs = event.target.querySelectorAll('input')
-
- 
-  $inputs.forEach(input => {
-      switch (input.id) {
-          case "email":
-              console.log("email");
-              if (!checkEmail(input.value)) errors.push([input.id, "L'email n'est pas valide"])
-              else user.email = input.value
-              break;
-          case "password":
-              console.log("password");
-               if (!checkPassword(input.value)) errors.push([input.id, "motdepasse n'est pas valide"])
-               else user.password = input.value
-              break;
-          case "birthDate":
-              console.log("birthDate");
-               if (!checkDate(input.value)) errors.push([input.id, "birthDate n'est pas valide"])
-               else user.birthDate = input.value
-              break;
-          default:
-              break;
-      }
-  });
 
 
-  if (errors.length > 0) {
+
+//   event.preventDefault()
+
+//   const $inputs = event.target.querySelectorAll('input')
+
+//   $inputs.forEach(input => {
+//       switch (input.id) {
+
+//           case "pseudo":
+//               console.log("pseudo");
+//               if (!checkEmail(input.value)) errors.push([input.id, "L'email n'est pas valide"])
+//               else user.email = input.value
+//               break;
+//           case "email":
+//               console.log("email");
+//               if (!checkEmail(input.value)) errors.push([input.id, "L'email n'est pas valide"])
+//               else user.email = input.value
+//               break;
+//           case "password":
+//               console.log("password");
+//                if (!checkPassword(input.value)) errors.push([input.id, "motdepasse n'est pas valide"])
+//                else user.password = input.value
+//               break;
+//           case "password-again":
+//               console.log("birthDate");
+//                if (!checkDate(input.value)) errors.push([input.id, "birthDate n'est pas valide"])
+//                else user.birthDate = input.value
+//               break;
+//           default:
+//               break;
+//       }
+//   });
+
+
+//   if (errors.length > 0) {
   
-      errors.forEach(error => {
+//       errors.forEach(error => {
          
-          const $displayErrorTarget = document.getElementById(`erreur-${error[0]}`)
-          $displayErrorTarget.innerHTML = ''
-          $displayErrorTarget.innerHTML = error[1]
-      })
-  } else {
+//           const $displayErrorTarget = document.getElementById(`erreur-${error[0]}`)
+//           $displayErrorTarget.innerHTML = ''
+//           $displayErrorTarget.innerHTML = error[1]
+//       })
+//   } else {
       
-      saveUser(user)
-      document.getElementById('message-succes').textContent = "User saved"
-  }
-}
+//       saveUser(user)
+//       document.getElementById('message-succes').textContent = "User saved"
+//   }
+// }
 
 
 
-function checkEmail(email) {
-  const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
-  return emailPattern.test(email);
-}
+// function checkEmail(email) {
+//   const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-z]{2,}$/;
+//   return emailPattern.test(email);
+// }
 
-function checkPassword(password) {
-  const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&!*])[A-Za-z\d@#$%^&!*]{8,}$/;
-  return passwordPattern.test(password);
-}
+// function checkPassword(password) {
+//   const passwordPattern = /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[@#$%^&!*])[A-Za-z\d@#$%^&!*]{8,}$/;
+//   return passwordPattern.test(password);
+// }
 
-function checkDate(date) {
+// function checkDate(date) {
   
-  const dateObject = new Date(date);
-  return !Number.isNaN(dateObject.getTime());
-}
+//   const dateObject = new Date(date);
+//   return !Number.isNaN(dateObject.getTime());
+// }
 
-function checkNumber(number) {
-  return !Number.isNaN(number)
-}
+// function checkNumber(number) {
+//   return !Number.isNaN(number)
+// }
 
 // End of form
 
